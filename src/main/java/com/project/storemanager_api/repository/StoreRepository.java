944aa9fab4bb83471f5ddf903d7b78fb10906685
hgb926 <hgb926@naver.com>
2025-02-09 10:32:01 +0900

    #16 [Feat] 유저가 만든 매장 전체조회 API 개발

--- src/main/java/com/project/storemanager_api/repository/StoreRepository.java
+++ src/main/java/com/project/storemanager_api/repository/StoreRepository.java
@@ -1,13 +1,20 @@
 package com.project.storemanager_api.repository;
 
 import com.project.storemanager_api.domain.store.dto.request.SaveStoreRequestDto;
+import com.project.storemanager_api.domain.store.dto.response.StoreResponseDto;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Mapper
 @Repository
 public interface StoreRepository {
 
+    // 매장 생성
     void saveStore(SaveStoreRequestDto dto);
+
+    // 한 유저의 매장 리스트 출력
+    List<StoreResponseDto> findStoreList(Long userId);
 
 }
--- src/main/java/com/project/storemanager_api/service/StoreService.java
+++ src/main/java/com/project/storemanager_api/service/StoreService.java
@@ -1,6 +1,7 @@
 package com.project.storemanager_api.service;
 
 import com.project.storemanager_api.domain.store.dto.request.SaveStoreRequestDto;
+import com.project.storemanager_api.domain.store.dto.response.StoreResponseDto;
 import com.project.storemanager_api.exception.ErrorCode;
 import com.project.storemanager_api.exception.StoreException;
 import com.project.storemanager_api.repository.StoreRepository;
@@ -9,6 +10,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional
@@ -27,14 +30,14 @@ public class StoreService {
      * 입력값 검증 후 이상없으면 password인코딩 후 db 저장
      */
     public void saveStore(SaveStoreRequestDto dto, Long userId) {
-
+        log.info("매장 생성 요청 : {}", userId);
         if (dto.getStoreName().isEmpty() || dto.getStorePlace().isEmpty() || dto.getPassword().isEmpty()) {
             log.info("입력값 비어있음!");
             throw new StoreException(ErrorCode.EMPTY_DATA, "가게 이름, 가게 장소는 필수 입력값입니다.");
         }
 
         if (dto.getPassword().length() != 4) {
-            throw new StoreException(ErrorCode.PASSWORD_TOO_SHORT, ErrorCode.PASSWORD_TOO_SHORT.getMessage());
+            throw new StoreException(ErrorCode.NOT_VALID_PASSWORD, ErrorCode.NOT_VALID_PASSWORD.getMessage());
         }
 
         String encodedPassword = passwordEncoder.encode(dto.getPassword());
@@ -42,5 +45,12 @@ public class StoreService {
 
         dto.setUserId(userId);
         storeRepository.saveStore(dto);
+    }
+
+    // 한 유저가 생성한 매장목록 전체 조회
+    @Transactional(readOnly = true)
+    public List<StoreResponseDto> getStoreList(Long userId) {
+        List<StoreResponseDto> storeList = storeRepository.findStoreList(userId);
+        return storeList;
     }
 }
