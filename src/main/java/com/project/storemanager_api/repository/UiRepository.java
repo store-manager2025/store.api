8b5b40c2711e02cd7ecb549232d60e78d3e58d24
hgb926 <hgb926@naver.com>
2025-02-09 15:27:44 +0900

    #21 [Test] Ui 저장 후 바로 id값 가져오기

--- src/main/java/com/project/storemanager_api/repository/UiRepository.java
+++ src/main/java/com/project/storemanager_api/repository/UiRepository.java
@@ -1,9 +1,12 @@
 package com.project.storemanager_api.repository;
 
+import com.project.storemanager_api.domain.ui.entity.UiLayout;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
 @Mapper
 @Repository
 public interface UiRepository {
+
+    void saveUi(UiLayout layout);
 }
