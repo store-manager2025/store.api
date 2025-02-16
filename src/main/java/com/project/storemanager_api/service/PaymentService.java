b3fc611325a0bb92ab5c66c18cbd9952fa9b4811
hgb926 <hgb926@naver.com>
2025-02-16 23:01:51 +0900

    #65-2 [Feat] Payment POST 요청 API 예외 처리
    긁어야 하는 돈보다, 요청 들어온 금액이 더 큰 경우 error로 간주

--- src/main/java/com/project/storemanager_api/service/PaymentService.java
+++ src/main/java/com/project/storemanager_api/service/PaymentService.java
@@ -5,8 +5,11 @@ import com.project.storemanager_api.domain.pay.dto.request.CreatePayRequestDto;
 import com.project.storemanager_api.domain.pay.dto.request.CreatePaymentDetailDto;
 import com.project.storemanager_api.exception.ErrorCode;
 import com.project.storemanager_api.exception.OrderException;
+import com.project.storemanager_api.exception.PaymentException;
+import com.project.storemanager_api.exception.PlaceException;
 import com.project.storemanager_api.repository.OrderRepository;
 import com.project.storemanager_api.repository.PaymentRepository;
+import com.project.storemanager_api.repository.PlaceRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -21,12 +24,20 @@ public class PaymentService {
     private final PaymentRepository paymentRepository;
     private final PaymentDetailService paymentDetailService;
     private final OrderRepository orderRepository;
+    private final PlaceRepository placeRepository;
 
 
     public void requestPayment(CreatePayRequestDto dto) {
         log.info("requestPayment의 DTO : {} ", dto.toString());
         // 주문 id가 유효한지 검증
-        checkOrderId(dto.getOrderId());
+        Order foundOrder = checkOrderId(dto.getOrderId());
+        // place id가 유효한지 검증
+        checkPlaceId(dto.getPlaceId());
+
+        // 긁어야 하는 돈보다, 요청 들어온 돈이 큰 경우
+        if (foundOrder.getPrice() < dto.getTotalAmount()) {
+            throw new PaymentException(ErrorCode.TOO_MUCH_PRICE, "필요한 금액을 초과합니다.");
+        }
 
         // 유효하다면 결제 정보 저장
         paymentRepository.savePayment(dto);
@@ -48,7 +59,13 @@ public class PaymentService {
 
     private Order checkOrderId(Long orderId) {
         return orderRepository.findById(orderId).orElseThrow(
-                () -> new OrderException(ErrorCode.INVALID_ID, "주문 내역을 찾을 수 없습니다")
+                () -> new OrderException(ErrorCode.INVALID_ID, "주문 내역을 찾을 수 없습니다.")
+        );
+    }
+
+    private void checkPlaceId(Long placeId) {
+        placeRepository.findById(placeId).orElseThrow(
+                () -> new PlaceException(ErrorCode.INVALID_ID, "장소 정보를 찾을 수 없습니다.")
         );
     }
 }
