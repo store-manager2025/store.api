94cdced4e1c3d06dbc2a5674a04e1503c00b2378
hgb926 <hgb926@naver.com>
2025-02-17 11:13:21 +0900

    #66-1 [Feat] Payment 매장별 전체 조회 GET 응답 컬럼 추가
    DB에서 쿼리로 paymentType 찾아옴

--- src/main/java/com/project/storemanager_api/domain/pay/dto/response/PaymentResponseDto.java
+++ src/main/java/com/project/storemanager_api/domain/pay/dto/response/PaymentResponseDto.java
@@ -5,6 +5,8 @@ import lombok.*;
 
 import java.time.LocalDateTime;
 
+import static com.project.storemanager_api.domain.pay.entity.PaymentDetail.PaymentType;
+
 @Getter
 @ToString
 @EqualsAndHashCode
@@ -19,8 +21,9 @@ public class PaymentResponseDto {
     private Integer paymentAmount;
     private Integer discountAmount;
     private String menuNames;
+    private PaymentType paymentType;
 
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime createdAt;
 
 
