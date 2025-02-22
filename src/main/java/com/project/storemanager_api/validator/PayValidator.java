efb172769df58ee780c368beb41d7dec21c9ffa3
hgb926 <hgb926@naver.com>
2025-02-22 16:17:11 +0900

    #89 [Feat] 결제요청 금액 초과, 미달 검증

--- src/main/java/com/project/storemanager_api/validator/PayValidator.java
+++ src/main/java/com/project/storemanager_api/validator/PayValidator.java
@@ -35,19 +35,13 @@ public class PayValidator {
         // place id가 유효한지 검증
         validatePlaceId(dto.getPlaceId());
 
-        Integer sumAmount = 0;
-        for (CreatePaymentDetailDto info : dto.getPayList()) {
-            sumAmount += info.getPaidMoney();
-        }
-        // 긁어야 하는 돈보다, 요청 들어온 돈이 큰 경우
-        if (foundOrder.getPrice() < sumAmount) {
-            throw new PaymentException(ErrorCode.TOO_MUCH_PRICE, "필요한 금액을 초과합니다.");
-        }
+        checkAmount(dto, foundOrder);
         for (CreatePaymentDetailDto payDetail : dto.getPayList()) {
             // payDetail.getExpiryDate() 날짜 확인
             checkExpiryDate(payDetail.getExpiryDate(), payDetail.getPaymentType());
         }
     }
+
 
     public void validateStoreId(Long storeId) {
         storeRepository.findPasswordById(storeId).orElseThrow(
@@ -105,6 +99,23 @@ public class PayValidator {
         int month = Integer.parseInt(parts[1]); // "03" → 3
         if (month < 1 || month > 12) {
             throw new PaymentException(ErrorCode.NOT_CORRECT_MONTH, ErrorCode.NOT_CORRECT_MONTH.getMessage());
+        }
+    }
+
+    private void checkAmount(CreatePayRequestDto dto, Order foundOrder) {
+        Integer sumAmount = 0;
+        for (CreatePaymentDetailDto info : dto.getPayList()) {
+            sumAmount += info.getPaidMoney();
+        }
+
+        // 금액 미달 경우
+        if (foundOrder.getPrice() > sumAmount) {
+            throw new PaymentException(ErrorCode.NOT_ENOUGH_PRICE, ErrorCode.NOT_ENOUGH_PRICE.getMessage());
+        }
+
+        // 금액 초과 경우
+        if (foundOrder.getPrice() < sumAmount) {
+            throw new PaymentException(ErrorCode.TOO_MUCH_PRICE, ErrorCode.TOO_MUCH_PRICE.getMessage());
         }
     }
 
