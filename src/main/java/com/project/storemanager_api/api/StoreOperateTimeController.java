6b838992f723ec762b84ab018fe05bc141a46e2f
hgb926 <hgb926@naver.com>
2025-02-24 14:16:47 +0900

    #104-1 [Feat] 영업 시작/마감 요청 POST API 소유주 검증

--- src/main/java/com/project/storemanager_api/api/StoreOperateTimeController.java
+++ src/main/java/com/project/storemanager_api/api/StoreOperateTimeController.java
@@ -1,9 +1,12 @@
 package com.project.storemanager_api.api;
 
+import com.project.storemanager_api.annotation.StoreAuthCheck;
+import com.project.storemanager_api.domain.user.dto.response.CustomUserPrincipal;
 import com.project.storemanager_api.service.StoreOperateTimeService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,16 +24,18 @@ public class StoreOperateTimeController {
     private final StoreOperateTimeService timeService;
 
     @PostMapping("/open/{storeId}")
-//    @StoreAuthCheck
-    public ResponseEntity<Map<String, Object>> openStore(@PathVariable Long storeId) {
+    @StoreAuthCheck
+    public ResponseEntity<Map<String, Object>> openStore(@AuthenticationPrincipal CustomUserPrincipal info,
+                                                         @PathVariable Long storeId) {
         log.info("open store : {}, {}", storeId, LocalDateTime.now());
         Map<String, Object> result = timeService.openStore(storeId);
         return ResponseEntity.ok().body(result);
     }
 
     @PostMapping("/close/{storeId}")
-//    @StoreAuthCheck
-    public ResponseEntity<Map<String, Object>> closeStore(@PathVariable Long storeId) {
+    @StoreAuthCheck
+    public ResponseEntity<Map<String, Object>> closeStore(@AuthenticationPrincipal CustomUserPrincipal info,
+                                                          @PathVariable Long storeId) {
         log.info("close store : {}, {}", storeId, LocalDateTime.now());
         Map<String, Object> result = timeService.closeStore(storeId);
         return ResponseEntity.ok().body(result);
