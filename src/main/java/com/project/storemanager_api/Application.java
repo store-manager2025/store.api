fa2781c37747f1641b0420f7bbeb3100cfec7203
hgb926 <hgb926@naver.com>
2025-03-21 17:34:12 +0900

    #164-1 [Chore] Github actions 구축 테스트 2

--- src/main/java/com/project/storemanager_api/Application.java
+++ src/main/java/com/project/storemanager_api/Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("Github action 구축 테스트");
+        System.out.println("Github action 구축 테스트 2");
         SpringApplication.run(Application.class, args);
     }
 
